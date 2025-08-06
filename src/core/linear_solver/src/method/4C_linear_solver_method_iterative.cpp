f286f4875400c562c76e5c7a49d87ba9b9ef4b61
Christoph Schmidt <christoph.schmidt@tum.de>
2025-08-06 09:35:42 +0200

    Some changes playing around with solver settings

--- src/core/linear_solver/src/method/4C_linear_solver_method_iterative.cpp
+++ src/core/linear_solver/src/method/4C_linear_solver_method_iterative.cpp
@@ -100,6 +100,10 @@ int Core::LinearSolver::IterativeSolver<MatrixType, VectorType>::solve()
     auto belosPrec =
         Teuchos::make_rcp<Belos::EpetraPrecOp>(Teuchos::rcp(preconditioner_->prec_operator()));
     problem->setRightPrec(belosPrec);
+    // one potential mitigation strategy for reducing the deviation between implicit and explicit
+    // residual is the choice of right preconditioning according to ChatGPT, but we already seem to
+    // do that.
+    // FOUR_C_THROW("We already set a right preconditioner!");
   }
 
   const bool set = problem->setProblem();
@@ -188,7 +192,7 @@ int Core::LinearSolver::IterativeSolver<MatrixType, VectorType>::solve()
 
     if (glob_error > 0)
     {
-      if (belist.get<bool>("THROW_IF_UNCONVERGED"))
+      if (false and belist.get<bool>("THROW_IF_UNCONVERGED"))
       {
         FOUR_C_THROW("Core::LinearSolver::BelosSolver: Iterative solver did not converge.");
       }
