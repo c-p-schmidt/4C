6cf7e50543eb336c89e2b7c9a2303c4189bcf5fe
Christoph Schmidt <christoph.schmidt@tum.de>
2025-09-04 09:28:04 +0200

    Remove the couplestress implementation
    as it is not actively used.

--- src/solid_poro_3D_ele/4C_solid_poro_3D_ele_calc_pressure_based.cpp
+++ src/solid_poro_3D_ele/4C_solid_poro_3D_ele_calc_pressure_based.cpp
@@ -252,19 +252,6 @@ void Discret::Elements::SolidPoroPressureBasedEleCalc<celltype>::coupling_stress
     const Core::Elements::Element& ele, const Core::FE::Discretization& discretization,
     const std::vector<int>& lm, Teuchos::ParameterList& params)
 {
-  auto iocouplingstress =
-      params.get<Inpar::Solid::StressType>("iocouplstress", Inpar::Solid::stress_none);
-
-  // check for output of coupling stress
-  if (iocouplingstress == Inpar::Solid::stress_none)
-  {
-    // nothing to do for calculation of effective stress
-    return;
-  }
-  else
-  {
-    FOUR_C_THROW("coupling stress poroelast not yet implemented for pressure-based variant");
-  }
 }
 
 // template classes
