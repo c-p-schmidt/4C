d51ee1204cf4d5496a88f891a0a6e09ca8690bc3
Christoph Schmidt <christoph.schmidt@tum.de>
2026-08-04 15:00:00 +0200

    The output of
    TotalTractionCorrectionCond and VolumetricSurfaceFlowCond are only
    used for restart purposes. At least there is no method to post process
    these quantities available.
    Thus, I renamed the methods to reflect this behavior

--- src/fluid/4C_fluid_timint_red.cpp
+++ src/fluid/4C_fluid_timint_red.cpp
@@ -11,6 +11,7 @@
 #include "4C_fem_condition_locsys.hpp"
 #include "4C_fluid_coupling_red_models.hpp"
 #include "4C_fluid_meshtying.hpp"
+#include "4C_fluid_utils_mapextractor.hpp"
 #include "4C_fluid_volumetric_surfaceFlow_condition.hpp"
 #include "4C_global_data.hpp"
 #include "4C_io.hpp"
@@ -314,21 +315,10 @@ void FLD::TimIntRedModels::setup_meshtying()
 
 /*----------------------------------------------------------------------*
  *----------------------------------------------------------------------*/
-void FLD::TimIntRedModels::write_output() const
-{
-  vol_surf_flow_bc_->output(*output_);
-  traction_vel_comp_adder_bc_->output(*output_);
-}
-
-/*----------------------------------------------------------------------*
- *----------------------------------------------------------------------*/
 void FLD::TimIntRedModels::write_restart() const
 {
-  if (step_ % upres_ != 0)
-  {
-    vol_surf_flow_bc_->output(*output_);
-    traction_vel_comp_adder_bc_->output(*output_);
-  }
+  vol_surf_flow_bc_->write_restart(*output_);
+  traction_vel_comp_adder_bc_->write_restart(*output_);
 
   // Check if one-dimensional artery network problem exist
   if (ART_timeInt_ != nullptr)
@@ -347,11 +337,7 @@ void FLD::TimIntRedModels::write_restart() const
 void FLD::TimIntRedModels::output()
 {
   FluidImplicitTimeInt::output();
-  // write solution
-  if (step_ % upres_ == 0)
-  {
-    write_output();
-  }
+
   // write restart
   if (uprestart_ > 0 && step_ % uprestart_ == 0)
   {
